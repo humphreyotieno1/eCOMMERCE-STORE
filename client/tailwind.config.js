c4533dbb61e2afd34aeb56b7be8e29794cc7560f
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-21 23:38:18 +0300

    Tailwind css configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-        raleway: ['Raleway', 'sans-serif']
-      }
-    },
+      extend: {
+          fontFamily: {
+              montserrat: ['Montserrat', 'sans-serif'],
+              raleway: ['Raleway', 'sans-serif']
+          }
+      },
   },
   plugins: [],
 }
