1ce91efcce39bbe97e8883b02a162e934627c03a
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-24 23:59:33 +0300

    Adding sample data

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -4,6 +4,7 @@ import Cart from './Cart.jsx';
 import { motion, AnimatePresence } from 'framer-motion';
 import Pagination from '../components/Pagination.jsx';
 
+// sample products
 const initialProducts = [
   {
     imageUrl: 'https://res.cloudinary.com/drdradtyj/image/upload/v1718628397/GEOCEL/Bamburi_Fundi.jpg',
